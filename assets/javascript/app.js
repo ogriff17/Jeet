7bfb7df4d202cdcc0d376824a6e14de7235c4d8b
Ken Dacanay <ken.p.r.dacanay@gmail.com>
2020-04-02 10:56:35 -0400

    see my slack messages, i switched apis to spoonacular, has many more options to display. was able to get modal to show recipe summary, but only 1 for now.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -30,7 +30,7 @@ $(document).ready(function () {
     function displayRecipes() {
         var food = $(this).attr("data-food");
 
-        var queryURL = "https://api.edamam.com/search?q=" + food + "&app_id=61b506d1&app_key=f0d00dcc2ff7622fe52c06579759bdd0&limit=20";
+        var queryURL = "https://api.spoonacular.com/recipes/complexSearch?apiKey=af0218eedd114716bee05b4c6cc69e87&addRecipeInformation=true&query=" + food;
 
         console.log(queryURL);
 
@@ -43,11 +43,11 @@ $(document).ready(function () {
                 $("#recipe-col-1").empty();
                 $("#recipe-col-2").empty();
                 $("#recipe-col-3").empty();
-                var results = response.hits;
+                var foodResults = response.results;
 
 
                 //If no information on search is available, alert the user. Need to turn this into a modal
-                if (results.length === 0) {
+                if (foodResults.length === 0) {
                     alert('Sorry, there are no recipe for this topic');
                     var itemindex = foodTopics.indexOf(food);
                     // otherwise display button
@@ -57,16 +57,29 @@ $(document).ready(function () {
                     }
                 }
                  
-                for (var j = 0; j < results.length; j++) {
+                for (var j = 0; j < foodResults.length; j++) {
                    
                     var foodDiv = $("<div>");
-                    
+                    var q = $("<h5>").text(foodResults[j].title);
+                    // var u = $("<href>").text(results[j].recipe.url);
+
+                    // var foodDiv = $("<a href=" + results[j].recipe.url + "</a>");
                     var foodImage = $("<img>");
-                    foodImage.attr("src", results[j].recipe.image);
-                    foodImage.attr("url", results[j].recipe.image);
+                    foodImage.attr("src", foodResults[j].image);
+                    foodImage.attr("url", foodResults[j].image);
                     foodImage.addClass("image");
                     foodDiv.append(foodImage);
                     foodDiv.addClass("card", "card-img-top", "card-body");
+                    foodDiv.append(q);
+                    // foodDiv.append(u);
+                    // u.attr("href");
+                    $(".modal-body").html(foodResults[j].summary);
+
+                    foodDiv.click(function() {
+                        console.log($(this));
+                        $("#modalWindow").modal("show");
+                        
+                     });
 
                     if (j >= 0 && j < 3) {
                         $("#recipe-col-1").append(foodDiv);
